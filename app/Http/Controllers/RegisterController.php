4585d4b68023c5f42325dff4fe6a939148fb7d6e
Elton Fonseca <elton869@gmail.com>
2021-10-04 14:15:34 -0300

    09 - Criando usuário no banco de dados

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    /**
+     * mostrar o formulário de cadastro de usuários
+     *
+     * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
+     */
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed']
+        ]);
+
+        $dados = $request->only(['name', 'email', 'password']);
+        $dados['password'] = Hash::make($dados['password']);
+
+        return User::create($dados);
+    }
+}
